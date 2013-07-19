68d857d0e5ff33250dabe266eb3355e93fc25bdd
Aaron <rokwar@ymail.com>
2013-07-19 08:18:56 -0600

    Commiting for reals this time

--- Deck.rb
+++ Deck.rb
@@ -1,3 +1,28 @@
+require_relative 'card'
 class Deck
-
+  attr_accessor :deck_cards
+  def initialize
+    @deck_cards = Array.new
+    fill_deck  
+  end
+  def fill_deck
+    13.times do |i|
+      @deck_cards.push(Card.new("S", i+1))
+    end
+    13.times do |i|
+      @deck_cards.push(Card.new("H", i+1))
+    end
+    13.times do |i|
+      @deck_cards.push(Card.new("C", i+1))
+    end
+    13.times do |i|
+      @deck_cards.push(Card.new("D", i+1))
+    end
+  end
+  def get_pocket_cards
+    @deck_cards.sample(2)
+  end
+  def get_dealt_cards
+    @deck_cards.sample(5)
+  end
 end
--- card.rb
+++ card.rb
@@ -1,5 +1,12 @@
 class Card
 
 	attr_accessor :suit, :value
+	def initialize(suit, value)
+	  @suit = suit
+	  @value = value
+	end
+	def to_s
+	  "#{@suit}#{@value}"
+	end
 
 end
