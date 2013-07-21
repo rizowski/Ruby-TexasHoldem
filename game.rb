43baa2febcd1bf8ee3442e4509cce5ad7ef5a043
Aaron <rokwar@ymail.com>
2013-07-21 17:19:07 -0600

    Checking for type of hand now returns points that relate to hand (higher is better)

--- game.rb
+++ game.rb
@@ -48,6 +48,7 @@ class Game
 		cards.sort_by! {|card| card.value}
 		first_suit = cards.first.suit
 		last_card_value = cards.first.value-1 # no bad ok oh well
+		last_card_value_for_matching = cards.first.value
 		straight_match = true
 
 		cards.each_with_index do |card,index|
@@ -55,19 +56,45 @@ class Game
 			straight_match = straight_match && ((card.value - last_card_value) == 1)
 			last_card_value = card.value
 		end
-		straight_match
+		count_of = Array.new
+		index_count = 0
+		while index_count < 5
+		  card_selected = cards[index_count]
+		  number_of = cards.select{|card| card.value == card_selected.value}.count
+		  index_count += number_of
+		  count_of.push(number_of)
+		end
+		if suit_match && straight_match
+		  return 10
+		elsif count_of.include?(4)
+		  return 9
+		elsif count_of.include?(3) && count_of.include?(2)
+		  return 8
+		elsif suit_match
+		  return 7
+		elsif straight_match
+		  return 6
+		elsif count_of.include?(3)
+		  return 5
+		elsif count_of.include?(2) && count_of.length == 3
+		  return 4
+		elsif count_of.include?(2)
+		  return 3
+		else
+		  return 0                
+		end
 	end
 end
 
 g = Game.new
 
 g.start
-puts g.find_point_value [Card.new("C",1),Card.new("D",2),Card.new("D",3),Card.new("D",4),Card.new("D",5)]
+puts g.find_point_value [Card.new("D",1),Card.new("C",12),Card.new("C",4),Card.new("C",3),Card.new("C",5)]
 
-2,2 #two of a kind
-22, 33 #2pair
-12345# straight
-DDDDD #flush
-D4,S4,C4,H4 #four of a kind
-DDD,SS #full house
-555 #three of a kind
+# 2,2 #two of a kind
+# 22, 33 #2pair
+# 12345# straight
+# DDDDD #flush
+# D4,S4,C4,H4 #four of a kind
+# DDD,SS #full house
+# 555 #three of a kind
